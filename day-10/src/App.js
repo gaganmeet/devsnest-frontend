f80f984d570d3490c2d8bdd91c832bf4e13ab993
Gaganmeet Bahri <gaganmeet.bahri96@gmail.com>
2021-06-25 19:15:37 +0530

    day 11 and 12

---
+++ day-10/src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+import burger from "./assets/burger.png";
+import corndog from "./assets/corn-dog.png";
+import frenchfries from "./assets/french-fries.png";
+import friedrice from "./assets/fried-rice.png";
+import cake from "./assets/cake.png";
+import fish from "./assets/fish.png";
+import pea from "./assets/pea.png";
+import banana from "./assets/bananas.png";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="grid">
+        <button id="burger">Burger</button>
+        <button id="fries">Fries</button>
+        <button id="corndog">Corn-dog</button>
+        <button id="rice">Rice</button>
+        <button id="fish">Fish</button>
+        <button id="pea">Pea</button>
+        <button id="banana">Banana</button>
+        <button id="cake">Cake</button>
+        <button id="burger">Burger</button>
+        <button id="fries">Fries</button>
+        <button id="corndog">Corn-dog</button>
+        <button id="rice">Rice</button>
+        <button id="fish">Fish</button>
+        <button id="pea">Pea</button>
+        <button id="banana">Banana</button>
+        <button id="cake">Cake</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
